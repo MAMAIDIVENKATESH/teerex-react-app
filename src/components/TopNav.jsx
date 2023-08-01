55aa2eb51d889b1345f6bc39779bcaca849cae03
MAMAIDIVENKATESH <mamidi096@gmail.com>
2023-08-01 13:20:29 +0530

    first commit

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './style.css'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { CartState } from '../context/context';
+  
+
+const TopNav = () => {
+    const { state: { cart } } = CartState()
+
+    return (
+        <nav className='nav-container'>
+            <div className='nav-inner'>
+                <div className='flex-cnt'>
+                    <Link to="/" className='link'>
+                        <div className="logo">TeeRex <span>Store</span></div>
+                    </Link>
+                    
+
+                </div>
+                <ul className='menus'>
+                    <Link to="/" className='link'>
+                        <li>Products</li>
+                    </Link>
+                    <Link to="/cart" className='link'>
+                        <li className='cart-icon'>
+                            {
+                                cart.length ? <div className='cart-count'>{cart.length}</div> : null
+                            }
+                            <AiOutlineShoppingCart />
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default TopNav
